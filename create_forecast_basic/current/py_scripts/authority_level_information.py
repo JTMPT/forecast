417a02d379e5b3af79254e22c919f84ed981229b
David Perelman <dperelman3@gmail.com>
2024-02-19 11:55:04 +0200

    DP create authority_level_information function

---
+++ create_forecast_basic/current/py_scripts/authority_level_information.py
@@ -0,0 +1,5 @@
+def authority_level_information(stat, stat_point, muni_under_JTMT_ITM):
+    stat=stat.set_index('STAT')
+    stat['CR_PNIM']=stat_point.sjoin(muni_under_JTMT_ITM)[['STAT','CR_PNIM']].set_index('STAT')
+    stat=stat.reset_index()
+    return stat
