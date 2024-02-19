f70affaa37a45530e39744f7c974c9e4f94e3f21
David Perelman <dperelman3@gmail.com>
2024-02-19 14:45:07 +0200

    DP some cleanup

--- create_forecast_basic/current/people/fix_cbs_data_230717/py_scripts/upload_population_information.py
+++ create_forecast_basic/current/people/fix_cbs_data_230717/py_scripts/upload_population_information.py
@@ -2,8 +2,11 @@ import pandas as pd
 
 def upload_population_information(stat, classification):
     col_name=['main_stat','classification_name']
+   
     classification=classification[col_name]
+   
     classification=classification.drop_duplicates(subset='main_stat',keep='first')
+   
     stat=pd.merge(stat,classification,on='main_stat',how='left')
 
     return stat
