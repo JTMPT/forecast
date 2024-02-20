704179713aa2c24f114c197bbc62768953683fb3
David Perelman <dperelman3@gmail.com>
2024-02-20 10:25:34 +0200

    DP students_current_year_230706: clean junk files

--- create_forecast_basic/current/people/students_current_year_230706/py_scripts/export_stat_area_level_information.py
+++
@@ -1,8 +0,0 @@
-def export_stat_area_level_information(stat):
-    stat=stat.fillna(0)
-
-    stat['pop_delta']=stat['pop']-stat['pop_cbs']
-
-    stat['aprt_20_delta']=stat['aprt_20']-stat['aprt_20_cbs']
-
-    return stat
