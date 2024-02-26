e3b0e1aaa753f974b60729661f83befc83a74e13
David Perelman <dperelman3@gmail.com>
2024-02-26 10:00:06 +0200

    DP: main - cleanup after merge

--- create_forecast_basic/current/people/fix_cbs_data_230717/py_scripts/authority_level_information.py
+++
@@ -1,7 +0,0 @@
-def authority_level_information(stat, stat_point, muni_under_JTMT_ITM):
-    stat=stat.set_index('STAT')
-    
-    stat['CR_PNIM']=stat_point.sjoin(muni_under_JTMT_ITM)[['STAT','CR_PNIM']].set_index('STAT')
-   
-    stat=stat.reset_index()
-    return stat
--- create_forecast_basic/current/people/students_current_year_230706/py_scripts/total_students.py
+++
@@ -1,8 +0,0 @@
-def total_students(taz):
-    taz=taz.fillna(0)
-
-    col=['student_for_Control','student_chardi_not_gov','student_arab_not_gov','student_toddlers']
-
-    taz['student']=taz[col].sum(axis=1)
-
-    return taz
