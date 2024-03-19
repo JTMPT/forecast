830290468c9ef28c05ad06d1f8e60c491891a2d9
David Perelman <dperelman3@gmail.com>
2024-03-19 11:36:44 +0200

    DP: future - change arab palastiniane folder

--- create_forecast_basic/future/arab_and_palestinian/run_arab_and_palestinian_from_future.py
+++ create_forecast_basic/future/arab_and_palestinian/run_arab_and_palestinian_from_future.py
@@ -6,14 +6,12 @@ path = os.getcwd()
 software_folder_location = r'{}\create_forecast_basic\current'.format(path)
 
 def run_notebook(notebook_path):
-    print('run!!!!!!!!')
     print(notebook_path)
 
     try:
         subprocess.check_call(['jupyter', 'nbconvert', '--to', 'notebook', '--execute', notebook_path])
         return True  # Execution successful
     except subprocess.CalledProcessError as e:
-        print('error!!!!!!!!')
         print(f"Error executing notebook: {e}")
         return False  # Execution failed
 
