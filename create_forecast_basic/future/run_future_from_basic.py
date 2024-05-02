77002d6b20b64a8e1a67bf939223041f32c0ef29
David Perelman <dperelman3@gmail.com>
2024-05-02 15:30:21 +0300

    DP: create_forecast_basic - run current and future together and compare to truth

---
+++ create_forecast_basic/future/run_future_from_basic.py
@@ -0,0 +1,15 @@
+import os
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
+
+def run_notebook(notebook_path):
+    with open(notebook_path) as ff:
+        nb_in = nbformat.read(ff, nbformat.NO_CONVERT)
+        
+    ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
+
+    nb_out = ep.preprocess(nb_in)
+
+path = os.getcwd()
+software_folder_location = r'{}\create_forecast_basic\future'.format(path)
+notebook_path = r'{}\run_future.ipynb'.format(software_folder_location)
