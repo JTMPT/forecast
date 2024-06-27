ad94ee0a9e42e59fb55bc16e39ea082685a59359
David Perelman <dperelman3@gmail.com>
2024-06-27 16:57:24 +0300

    DP: create_forecast_ad_hoc - change_Muni_Heb_to_Muni_Eng

--- create_forecast_ad_hoc/py_scripts/export_geo_layer_for_client_control.py
+++ create_forecast_ad_hoc/py_scripts/export_geo_layer_for_client_control.py
@@ -3,7 +3,7 @@ import pandas as pd
 def export_geo_layer(forecast, client_data_folder_location,file_date):
     save_shp_path=r'{}\For_approval\{}_taz_for_approval.shp'.format(client_data_folder_location,file_date)
     
-    col=['Taz_num','Name_hebre', 'main_sector', 'classification_name','Student_by_Classrooms','geometry']
+    col=['Taz_num','Name_hebre','Muni_Eng', 'main_sector', 'classification_name','Student_by_Classrooms','geometry']
 
     
     forecast[col].to_file(save_shp_path,index=False,encoding='UTF-8')
