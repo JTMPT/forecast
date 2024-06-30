c3bf8142c09bff9456e0803214a4d4da75030178
David Perelman <dperelman3@gmail.com>
2024-06-30 16:59:06 +0300

    DP: create_forecast_ad_hoc - main.ipynb - cleanup

--- create_forecast_ad_hoc/py_scripts/forecast.py
+++
@@ -1,8 +0,0 @@
-from functions import up_load_gdb
-
-def clientTaz(client_data_folder_location):
-    folder_path=r'{}\For_approval\Reference_tabels\shp'.format(client_data_folder_location)
-    gpd_name='tochnit_check.gdb'
-
-    forecast = up_load_gdb(r'{}\{}'.format(folder_path,gpd_name),'TAZ_211028_V3_Published_with_client_changes')
-    return forecast
