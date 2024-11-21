0711287194d48a788c6eb871b0587375aede0d25
David Perelman <dperelman3@gmail.com>
2024-11-21 14:47:51 +0200

    DP - create_forecast_ad_hoc - main.ipynb - Adding geographic features to taz

--- create_forecast_ad_hoc/global_functions.py
+++ create_forecast_ad_hoc/global_functions.py
@@ -1,6 +1,25 @@
 import fiona
 import geopandas as gpd
 import pandas as pd
+import os
+
+def find_files_with_pattern(folder_path, pattern):
+    """
+    Find files in a directory that match a certain pattern.
+    
+    Args:
+    - directory (str): The directory path.
+    - pattern (str): The pattern to search for in file names.
+    
+    Returns:
+    - List of file paths matching the pattern.
+    """
+    files = []
+    for root, _, filenames in os.walk(folder_path):
+        for filename in filenames:
+            if pattern in filename:
+                files.append(os.path.join(root, filename))
+    return files
 
 def up_load_gdb(path, layer_name):
     path='{}'.format(path)
