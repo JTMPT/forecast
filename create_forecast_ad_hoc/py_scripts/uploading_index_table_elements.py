36a4973c52f92fe3d12960b3bf289c680ebf692c
David Perelman <dperelman3@gmail.com>
2024-11-27 12:04:39 +0200

    DP - create_forecast_ad_hoc - main.ipynb - run all and cleanup

--- create_forecast_ad_hoc/py_scripts/uploading_index_table_elements.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from functions import split_index_by_taz, up_load_df, up_load_shp
-
-def uploading_index_table(forecast, client_data_folder_location, index_file_name):
-    borders_index=up_load_shp(r'{}\For_approval\Reference_tabels\shp\gvul_index.shp'.format(client_data_folder_location))
-    index=up_load_df(r'{}\For_approval\Reference_tabels'.format(client_data_folder_location),index_file_name)
-    index=pd.merge(borders_index,index,on='id',how='right')
-   
-    return index
